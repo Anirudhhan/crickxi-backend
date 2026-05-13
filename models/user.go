a5d83625c65de75a20711de40d5147aa2781ca6d
Anirudhhan <anirudhhan.ashok@gmail.com>
2026-05-13 15:30:00 +0530

    login user implemented

--- models/user.go
+++ models/user.go
@@ -5,3 +5,13 @@ type RegisterUser struct {
 	Phone    string `db:"phone" json:"phone" binding:"required"`
 	Password string `db:"password" json:"password" binding:"required,min=8"`
 }
+
+type LoginUserDetails struct {
+	UserID       string `db:"id"`
+	HashPassword string `db:"password"`
+}
+
+type LoginUser struct {
+	Phone    string `db:"phone" json:"phone" binding:"required"`
+	Password string `db:"password" json:"password" binding:"required"`
+}
