ac4e21b374c96ebadcc0bc0bc98eb246de7b88a8
Anirudhhan <anirudhhan.ashok@gmail.com>
2026-05-15 10:06:51 +0530

    schema field update and cors setup

--- routes/routes.go
+++ routes/routes.go
@@ -4,13 +4,27 @@ import (
 	"crickxi-backend/handler"
 	"crickxi-backend/middleware"
 	"net/http"
+	"os"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
 func SetUpRoutes() *gin.Engine {
 	router := gin.Default()
-	v1 := router.Group("v1")
+	var ipOrigins = os.Getenv("IP_ORIGIN")
+
+	router.Use(cors.New(cors.Config{
+		AllowOrigins: []string{
+			"http://localhost:5173",
+			ipOrigins,
+		},
+		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowHeaders:     []string{"Origin", "Content-Type", "Accept", "Authorization"},
+		AllowCredentials: true,
+	}))
+
+	v1 := router.Group("/v1")
 
 	{
 		v1.GET("/health", func(ctx *gin.Context) {
@@ -18,6 +32,7 @@ func SetUpRoutes() *gin.Engine {
 				"message": "server is running",
 			})
 		})
+
 		v1.POST("/register", handler.RegisterUser)
 		v1.POST("/login", handler.LoginUser)
 		v1.GET("/refresh", handler.RefreshToken)
