7bae8c9d33b3fcb15d6e35a54aae4930266d1f85
Anirudhhan <anirudhhan.ashok@gmail.com>
2026-05-22 17:16:15 +0530

    get match by id implemented

--- database/dbHelper/matches.go
+++ database/dbHelper/matches.go
@@ -47,6 +47,8 @@ func CreateMatch(tx *sqlx.Tx, req models.CreateMatchRequest, hostID string, toss
 func GetMatches() (matches []models.MatchCard, err error) {
 	query := `SELECT
 				m.id AS match_id,
+				m.toss_winner_team_id AS toss_winner_team_id,
+				m.host_id AS host_id,
 				ta.id AS team_a_id,
 				ta.name AS team_a_name,
 				tb.id AS team_b_id,
@@ -94,4 +96,104 @@ func GetMatches() (matches []models.MatchCard, err error) {
 
 	err = database.DB.Select(&matches, query)
 	return matches, err
+}
+
+func GetMatchByID(matchID string) (matchCard models.MatchCard, err error) {
+	query := `
+			SELECT
+				m.id AS match_id,
+				m.toss_winner_team_id AS toss_winner_team_id,
+				m.winner_team_id AS winner_team_id,
+				m.toss_decision AS toss_decision,
+				m.host_id AS host_id,
+				m.scorer1_id AS scorer1_id,
+				m.scorer2_id AS scorer2_id,
+				m.current_inning_no AS current_inning_no,
+				m.match_status AS match_status,
+				m.overs_per_side AS overs_per_side,
+				m.start_time AS start_time,
+				m.end_time AS end_time,
+				m.updated_at AS match_updated_at,
+				ta.id AS team_a_id,
+				ta.name AS team_a_name,
+				tb.id AS team_b_id,
+				tb.name AS team_b_name,
+				COALESCE(lm.current_score, 0)
+					AS current_score,
+				COALESCE(lm.wickets, 0)
+					AS wickets,
+				COALESCE(lm.legal_balls, 0)
+					AS legal_balls,
+			    COALESCE(pi.total_runs, 0)
+				AS previous_innings_score,
+				COALESCE(pi.legal_balls, 0)
+					AS previous_innings_legal_balls,
+				lm.current_bowler_id AS bowler_id,
+				lm.current_inning_id AS current_inning_id,
+				lm.striker_id AS striker_id,
+				lm.non_striker_id AS non_striker_id,
+				su.name AS striker_name,
+				bsc.runs AS striker_runs,
+				bsc.balls AS striker_balls,
+				nsu.name AS non_striker_name,
+				bnsc.runs AS non_striker_runs,
+				bnsc.balls AS non_striker_balls,
+				bu.name AS bowler_name,
+				bwsc.runs_given AS bowler_runs_given,
+				bwsc.legal_balls AS bowler_legal_balls,
+				bwsc.wickets AS bowler_wickets
+			
+			FROM matches m
+			
+			JOIN teams ta
+				ON ta.id = m.team_a_id
+			
+			JOIN teams tb
+				ON tb.id = m.team_b_id
+			
+			LEFT JOIN live_match lm
+				ON lm.match_id = m.id
+			    
+			LEFT JOIN innings pi
+				ON pi.match_id = m.id
+				AND pi.innings_order = m.current_inning_no - 1
+			
+			LEFT JOIN player_stats sps
+				ON lm.striker_id = sps.id
+			
+			LEFT JOIN users su
+				ON sps.user_id = su.id
+			
+			LEFT JOIN batting_scorecards bsc
+				ON bsc.player_id = sps.id
+				AND lm.current_inning_id = bsc.innings_id
+			
+			LEFT JOIN player_stats nsps
+				ON lm.non_striker_id = nsps.id
+			
+			LEFT JOIN users nsu
+				ON nsps.user_id = nsu.id
+			
+			LEFT JOIN batting_scorecards bnsc
+				ON bnsc.player_id = nsps.id
+				AND lm.current_inning_id = bnsc.innings_id
+			
+			LEFT JOIN player_stats bps
+				ON lm.current_bowler_id = bps.id
+			
+			LEFT JOIN users bu
+				ON bps.user_id = bu.id
+			
+			LEFT JOIN bowling_scorecards bwsc
+				ON bwsc.player_id = bps.id
+				AND lm.current_inning_id = bwsc.innings_id
+			
+			WHERE
+				m.id = $1
+				AND m.archived_at IS NULL
+			
+			ORDER BY m.created_at DESC`
+
+	err = database.DB.Get(&matchCard, query, matchID)
+	return matchCard, err
 }
--- models/match.go
+++ models/match.go
@@ -35,6 +35,26 @@ type CreateMatchRequest struct {
 type MatchCard struct {
 	MatchID string `db:"match_id" json:"matchID"`
 
+	TossWinnerTeamID string  `db:"toss_winner_team_id" json:"tossWinnerTeamID"`
+	WinnerTeamID     *string `db:"winner_team_id" json:"winnerTeamID"`
+
+	TossDecision *string `db:"toss_decision" json:"tossDecision"`
+
+	HostID    string  `db:"host_id" json:"hostID"`
+	Scorer1ID *string `db:"scorer1_id" json:"scorer1ID"`
+	Scorer2ID *string `db:"scorer2_id" json:"scorer2ID"`
+
+	CurrentInningNo *int `db:"current_inning_no" json:"currentInningNo"`
+
+	MatchStatus string `db:"match_status" json:"matchStatus"`
+
+	OversPerSide int `db:"overs_per_side" json:"oversPerSide"`
+
+	StartTime *time.Time `db:"start_time" json:"startTime"`
+	EndTime   *time.Time `db:"end_time" json:"endTime"`
+
+	MatchUpdatedAt *time.Time `db:"match_updated_at" json:"matchUpdatedAt"`
+
 	TeamAID   string `db:"team_a_id" json:"teamAID"`
 	TeamAName string `db:"team_a_name" json:"teamAName"`
 
@@ -45,19 +65,24 @@ type MatchCard struct {
 	Wickets      int `db:"wickets" json:"wickets"`
 	LegalBalls   int `db:"legal_balls" json:"legalBalls"`
 
-	MatchStatus string `db:"match_status" json:"matchStatus"`
+	PreviousInningsScore      *int `db:"previous_innings_score" json:"previousInningsScore"`
+	PreviousInningsLegalBalls *int `db:"previous_innings_legal_balls" json:"previousInningsLegalBalls"`
 
-	OversPerSide int `db:"overs_per_side" json:"oversPerSide"`
-
-	StartTime time.Time `db:"start_time" json:"startTime"`
+	CurrentInningID *string `db:"current_inning_id" json:"currentInningID"`
 
 	StrikerID    *string `db:"striker_id" json:"strikerID"`
 	StrikerName  *string `db:"striker_name" json:"strikerName"`
 	StrikerRuns  *int    `db:"striker_runs" json:"strikerRuns"`
 	StrikerBalls *int    `db:"striker_balls" json:"strikerBalls"`
 
-	BowlerID        *string `db:"bowler_id" json:"bowlerID "`
-	BowlerName      *string `db:"bowler_name" json:"bowlerName"`
-	BowlerWickets   *int    `db:"bowler_wickets" json:"bowlerWickets"`
-	BowlerRunsGiven *int    `db:"bowler_runs_given" json:"bowlerRunsGiven"`
+	NonStrikerID    *string `db:"non_striker_id" json:"nonStrikerID"`
+	NonStrikerName  *string `db:"non_striker_name" json:"nonStrikerName"`
+	NonStrikerRuns  *int    `db:"non_striker_runs" json:"nonStrikerRuns"`
+	NonStrikerBalls *int    `db:"non_striker_balls" json:"nonStrikerBalls"`
+
+	BowlerID         *string `db:"bowler_id" json:"bowlerID"`
+	BowlerName       *string `db:"bowler_name" json:"bowlerName"`
+	BowlerRunsGiven  *int    `db:"bowler_runs_given" json:"bowlerRunsGiven"`
+	BowlerLegalBalls *int    `db:"bowler_legal_balls" json:"bowlerLegalBalls"`
+	BowlerWickets    *int    `db:"bowler_wickets" json:"bowlerWickets"`
 }
